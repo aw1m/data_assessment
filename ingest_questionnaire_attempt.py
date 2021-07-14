a21c99dbdd0cddd35cc75743022b71b4525d3631
aw1m <44844222+aw1m@users.noreply.github.com>
2021-07-14 16:06:43 -0500

    everything loads

--- ingest_questionnaire_attempt.py
+++ ingest_questionnaire_attempt.py
@@ -21,17 +21,29 @@ def ingest_questionnaire_attempt(cursor, sqliteConnection):
         submitted_at, created_at, updated_at, deleted_at, context_uuid, \
         question_uuid, confidence, type, single_answer_choice_uuid = questionnaire_attempt.values()
         cursor.execute("""
-            SELECT *
-            FROM questionnaire_attempt q
-            WHERE  q.user_uuid=?
-            and q.activity_uuid=?
-            and q.questionnaire_uuid=?
-            and q.attempt_number=?
-            and q.created_at =?
-            and q.context_uuid=?
-            and q.question_uuid=?
-            and q.single_answer_choice_uuid=?
-         """, [user_uuid, activity_uuid, questionnaire_uuid, attempt_number, created_at, context_uuid,question_uuid, single_answer_choice_uuid])
+             SELECT *
+             FROM questionnaire_attempt q
+             WHERE  q.user_uuid=?
+        and q.activity_uuid=?
+        and q.questionnaire_uuid=?
+        and q.attempt_number=?
+        -- and q.submitted_at=?
+        and q.created_at =?
+        -- and ( q.updated_at <= ?
+        -- or q.deleted_at<= ?)
+        and q.context_uuid=?
+        and q.question_uuid=?
+        -- and q.confidence=?
+        -- and q.type=?
+        and q.single_answer_choice_uuid=?
+
+         """, [user_uuid, activity_uuid, questionnaire_uuid, attempt_number,
+        # submitted_at,
+               created_at,
+               # updated_at, deleted_at,
+        context_uuid,question_uuid,
+               # confidence, type,
+               single_answer_choice_uuid])
         records = cursor.fetchall()
 
         if records == []:
@@ -44,26 +56,33 @@ def ingest_questionnaire_attempt(cursor, sqliteConnection):
                                 updated_at, deleted_at])
                 sqliteConnection.commit()
             except Exception as e:
+                print("records == []")
                 print(e)
+                print(user_uuid, activity_uuid, questionnaire_uuid, context_uuid, question_uuid,
+                      single_answer_choice_uuid, attempt_number, confidence, type, submitted_at, created_at, updated_at,
+                      deleted_at)
+                print(records)
+
                 not_processed.append(questionnaire_attempt)
         else:
-            db_updated_at = records[0][12]
+            db_updated_at = records[0][11]
             if db_updated_at is None and updated_at is not None:
                 try:
                     cursor.execute("""
-                            delete 
-                            FROM questionnaire_attempt 
-                            WHERE  user_uuid=?
-                            and activity_uuid=?
-                            and questionnaire_uuid=?
-                            and attempt_number=?
-                            and created_at =?
-                            and updated_at is Null
-                            and context_uuid=?
-                            and question_uuid=?
-                            and single_answer_choice_uuid=?;
-                             """,
-                                   [user_uuid,activity_uuid,questionnaire_uuid,attempt_number,created_at,context_uuid,question_uuid,single_answer_choice_uuid])
+                                            delete 
+                                            FROM questionnaire_attempt 
+                                            WHERE  user_uuid=?
+                                            and activity_uuid=?
+                                            and questionnaire_uuid=?
+                                            and attempt_number=?
+                                            and created_at =?
+                                            and updated_at is Null
+                                            and context_uuid=?
+                                            and question_uuid=?
+                                            and single_answer_choice_uuid=?;
+                                             """,
+                                   [user_uuid, activity_uuid, questionnaire_uuid, attempt_number,created_at,
+                                    context_uuid, question_uuid,single_answer_choice_uuid])
                     sqliteConnection.commit()
                     cursor.execute("""
                          insert into questionnaire_attempt
@@ -74,23 +93,29 @@ def ingest_questionnaire_attempt(cursor, sqliteConnection):
                                     updated_at, deleted_at])
                     sqliteConnection.commit()
                 except Exception as e:
+                    print("db_updated_at is None and updated_at is not None")
                     print(e)
+
+                    print(user_uuid, activity_uuid, questionnaire_uuid, context_uuid, question_uuid,
+                          single_answer_choice_uuid, attempt_number, confidence, type, submitted_at, created_at,
+                          updated_at, deleted_at)
+                    print(records)
                     not_processed.append(questionnaire_attempt)
             elif  db_updated_at is not None and db_updated_at < updated_at:
                 try:
                     cursor.execute("""
-                            delete 
-                            FROM questionnaire_attempt 
-                            WHERE  user_uuid=?
-                            and activity_uuid=?
-                            and questionnaire_uuid=?
-                            and attempt_number=?
-                            and created_at =?
-                            and updated_at <?
-                            and context_uuid=?
-                            and question_uuid=?
-                            and single_answer_choice_uuid=?;
-                             """,
+                                        delete 
+                                        FROM questionnaire_attempt 
+                                        WHERE  user_uuid=?
+                                        and activity_uuid=?
+                                        and questionnaire_uuid=?
+                                        and attempt_number=?
+                                        and created_at =?
+                                        and updated_at <?
+                                        and context_uuid=?
+                                        and question_uuid=?
+                                        and single_answer_choice_uuid=?;
+                                         """,
                                    [user_uuid, activity_uuid, questionnaire_uuid, attempt_number, created_at,updated_at,
                                     context_uuid, question_uuid, single_answer_choice_uuid])
                     sqliteConnection.commit()
@@ -100,7 +125,10 @@ def ingest_questionnaire_attempt(cursor, sqliteConnection):
                                    [user_uuid ,activity_uuid ,questionnaire_uuid ,context_uuid ,question_uuid ,single_answer_choice_uuid ,attempt_number ,confidence ,type ,submitted_at  ,created_at ,updated_at ,deleted_at ])
                     sqliteConnection.commit()
                 except Exception as e:
+                    print("db_updated_at is not None and db_updated_at < updated_at")
                     print(e)
+                    print(user_uuid ,activity_uuid ,questionnaire_uuid ,context_uuid ,question_uuid ,single_answer_choice_uuid ,attempt_number ,confidence ,type ,submitted_at  ,created_at ,updated_at ,deleted_at)
+                    print(records)
                     not_processed.append(questionnaire_attempt)
             else:
                 not_processed.append(questionnaire_attempt)
